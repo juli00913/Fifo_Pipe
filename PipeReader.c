35ca2e0a325f96613038924e6c62b12338542d06
July Slavchev <jul.slavhev.tues19@gmail.com>
2017-12-03 22:33:55 +0200

    Commiting

---
+++ PipeReader.c
@@ -0,0 +1,20 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main()
+{
+		int fd = open("fifoPipe", O_RDONLY);
+		char buff[26];	
+		while(read(fd, buff, 26)) {
+			write(STDOUT_FILENO, buff, 26);
+			write(STDOUT_FILENO, "\n", 1);
+		}
+	
+	close(fd);
+	return 0;
+}
---
+++ PipeWriter.c
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main() {
+
+	mkfifo("fifoPipe", 0666); 
+	int fd = open("fifoPipe", O_WRONLY);
+	char* s = "Greetings from PipeWriter";
+	while(1) {
+		write( fd, s, strlen(s) );
+		sleep(1);
+	}
+	return 0;
+}
