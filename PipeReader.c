d389f1a331e08286139a8d4e90b2352baaa497ff
July Slavhev <jul.slavchev.tues19@gmail.com>
2017-12-10 21:18:26 +0200

    Update PipeReader.c

--- PipeReader.c
+++ PipeReader.c
@@ -8,12 +8,12 @@
 
 int main()
 {
-		int fd = open("fifoPipe", O_RDONLY);
-		char buff[26];	
-		while(read(fd, buff, 26)) {
-			write(STDOUT_FILENO, buff, 26);
-			write(STDOUT_FILENO, "\n", 1);
-		}
+	int fd = open("fifoPipe", O_RDONLY);
+	char buff[26];	
+	while(read(fd, buff, 26)) {
+		write(STDOUT_FILENO, buff, 26);
+		write(STDOUT_FILENO, "\n", 1);
+	}
 	
 	close(fd);
 	return 0;
